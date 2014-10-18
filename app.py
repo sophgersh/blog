50791af8b0d795f805e24f0263e359b4e59d0008
Justin Kim <yulli@yulli.org>
2014-10-18 17:34:09 -0400

    Add connection closing when the app exits

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, g, render_template
 
 import db
 
@@ -14,6 +14,14 @@ def index():
 def post(slug):
     post = db.find_post(slug)
     return render_template('post.html', d={'post': post})
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 if __name__ == '__main__':
     app.debug = True

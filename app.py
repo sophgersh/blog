c8ae3e1f15f6baba0a2ec2f3e542561ba28e8334
Justin Kim <yulli@yulli.org>
2014-10-18 16:13:53 -0400

    Add basic framework for work

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/<title>')
+def post(title):
+    post = find_post(title)
+    return render_template('post.html', d={'post': post})
+
+
+def find_post(title):
+    pass
+
 if __name__ == '__main__':
     app.debug = True
     app.run()

4cf6027920fe7f4381135c950f4f34ec11d3a329
Justin Kim <yulli@yulli.org>
2014-10-18 15:51:47 -0400

    Housekeeping

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    render_template("index.html")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

e661b3d13bc237ae3bb073f4438cb6ad2f65d1be
Justin Kim <yulli@yulli.org>
2014-10-18 16:43:06 -0400

    Add db.py for handling the sqlite database

---
+++ db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('blog.db')
+c = conn.cursor()
+
+
+def find_post(title):
+    query = 'SELECT * FROM blogs WHERE title = ?'
+    posts = c.execute(query, title)
+    return posts.fetchone()
+
+# Use this only to initialize a new database for the first time
+def setup():
+    queries = [
+                '''CREATE TABLE posts (
+                id integer primary key,
+                title text,
+                content text,
+                slug text
+                )'''
+    ]
+
+    for query in queries:
+        c.execute(query)
+
+    c.commit()
